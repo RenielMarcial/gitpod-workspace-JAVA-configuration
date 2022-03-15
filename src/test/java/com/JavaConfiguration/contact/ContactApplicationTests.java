27a36dbd91d4e2462333c693e2e8c439a1ea9493
RenielMarcial <toro.reniel.marcial@gmail.com>
2022-03-15 07:21:57 +0000

    updaated

--- src/test/java/com/JavaConfiguration/contact/ContactApplicationTests.java
+++ src/test/java/com/JavaConfiguration/contact/ContactApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.JavaConfiguration.contact;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ContactApplicationTests {
 
 	@Test
 	void contextLoads() {
